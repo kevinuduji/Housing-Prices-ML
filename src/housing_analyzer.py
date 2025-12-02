1bbc5bf7d0562d1cb486271ee4f0de78fd0176f3
Santosh Karthik <santoshnandivada6@gmail.com>
2025-12-02 16:48:29 -0600

    Setup housing analyzer

--- src/housing_analyzer.py
+++ src/housing_analyzer.py
@@ -1,5 +1,15 @@
-# hello world
-print ("Hello, World!")
+"""
+House Price Prediction using Linear Regression
+================================================
+This script predicts the SalePrice of houses using Linear Regression
+Identifies the top 3 most influential features.
+"""
 
-print("This is a housing analyzer script.")
-print("This push was made by Kevin")
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.preprocessing import LabelEncoder
+import pickle
+from matplotlib import pyplot as plt
